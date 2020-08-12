565c0d0c4f027f10440565da353b575a8c730b90
onuronen <oonen@purdue.edu>
2020-08-12 15:21:57 -0400

    config

---
+++ config.py
@@ -0,0 +1,7 @@
+postgres_config = {
+    'host' : 'ec2-3-223-21-106.compute-1.amazonaws.com',
+    'port' : '5432',
+    'database' : 'd1fl2tqleh7b65',
+    'user' : 'kdvezmofpzbiru',
+    'password' : '7341f2c169527a9e5761b49d18beb629b7f537f7d05e0baabbaf92f9e70526e4'
+}
