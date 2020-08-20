c14e7b1f885e3dadc46eeda591e36589c314e41f
onuronen <oonen@purdue.edu>
2020-08-20 18:05:43 -0400

    import errors

--- src/db/utils.py
+++ src/db/utils.py
@@ -2,8 +2,8 @@ import pandas as pd
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
-#from src.db.crud import update_table,fetch_rows
-#from src.db.models import Problem
+from src.db.crud import update_table,fetch_rows
+from src.db.models import Problem
 
 def process_ticket(short_description, description):
 
